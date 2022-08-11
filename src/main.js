c0ba7bca233fab884f2afd5994b2412afddbb621
felipe123981 <filipe.timachine@gmail.com>
2022-08-11 11:55:25 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,4 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-createApp(App).use(router).use(BootstrapVue).use(IconsPlugin).mount("#app");
+createApp(App).use(router).mount("#app");
